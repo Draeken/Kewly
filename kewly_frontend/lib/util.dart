0f69a312afd92ea089ecd8a2dec749c9fa017e5c
Draeken <pbm764927@gmail.com>
2020-05-17 22:54:42 +0200

    chore: add memo function

--- kewly_frontend/lib/util.dart
+++ kewly_frontend/lib/util.dart
@@ -23,3 +23,48 @@ class Mtransform {
 
   static final Float64List mirrorX = (Matrix4.identity()..[0] = -1).storage;
 }
+
+typedef R Func1<A, R>(A a);
+typedef R Func2<A, B, R>(A a, B b);
+
+/// Checks 1 argument for equality with [identical] call and returns the cached
+/// result if it was not changed.
+Func1<A, R> imemo1<A, R>(Func1<A, R> func) {
+  A prevArg;
+  R prevResult;
+  bool isInitial = true;
+
+  return ((A arg) {
+    if (!isInitial && identical(arg, prevArg)) {
+      return prevResult;
+    } else {
+      prevArg = arg;
+      prevResult = func(arg);
+      isInitial = false;
+
+      return prevResult;
+    }
+  });
+}
+
+/// Checks 2 arguments for equality with [identical] call and returns the cached
+/// result if they were not changed.
+Func2<A, B, R> imemo2<A, B, R>(Func2<A, B, R> func) {
+  A prevArgA;
+  B prevArgB;
+  R prevResult;
+  bool isInitial = true;
+
+  return ((A argA, B argB) {
+    if (!isInitial && identical(argA, prevArgA) && identical(argB, prevArgB)) {
+      return prevResult;
+    } else {
+      prevArgA = argA;
+      prevArgB = argB;
+      prevResult = func(argA, argB);
+      isInitial = false;
+
+      return prevResult;
+    }
+  });
+}
